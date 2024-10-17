99a3c17ddc5bebb6558ff829a0790a5bc8f394ec
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-10-17 18:20:31 +0100

    fix: hono + nextjs routing integration

--- src/features/auth/hooks/use-login.ts
+++ src/features/auth/hooks/use-login.ts
@@ -5,8 +5,6 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { client } from '@/lib/rpc';
 
-
-
 type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>
 type RequestType = InferRequestType<typeof client.api.auth.login["$post"]>
 
